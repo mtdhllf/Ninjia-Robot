67f90113d2056ea596e2ce27bcdc2b0a6214ac78
mtdhllf <1228551440@qq.com>
2019-10-29 17:51:10 +0800

    更新sdk,增加定时功能

--- app.go
+++ app.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Tnze/CoolQ-Golang-SDK/cqp"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/robfig/cron"
 	"strings"
 	"time"
 )
@@ -18,6 +19,8 @@ func main() { /*此处应当留空*/ }
 
 var db, err = gorm.Open("postgres", "postgres", "host=94.191.102.44 port=5432 user=postgres dbname=robot password=postgres sslmode=disable")
 
+var c = cron.New()
+
 func init() {
 	cqp.AppID = "me.cqp.mtdhllf.ninja.robot"
 	cqp.PrivateMsg = onPrivateMsg
@@ -30,6 +33,14 @@ func init() {
 	// 自动迁移模式
 	db.AutoMigrate(&Key{})
 
+	//定时器1(7-23时,每小时执行一次)
+	spec1 := "0 0 7-23/1 * * ?"
+	err = c.AddFunc(spec1, testJob)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	c.Start()
 }
 
 /**私聊入口*/
@@ -57,9 +68,11 @@ type Key struct {
 //<editor-fold defaultstate="collapsed" desc="私聊">
 func onKeyPrivateMsg(subType, msgID int32, fromQQ int64, msg string, font int32) int32 {
 	code := int32(0)
-	if strings.HasPrefix(msg, "兑换码 ") {
-		//本周查询
-		if strings.Contains(msg, "本周") || strings.Contains(msg, "这周") || strings.Contains(msg, "查询") {
+	if strings.Contains(msg, "兑换码") {
+		switch {
+		//查询
+		case strings.Contains(msg, "本周") || strings.Contains(msg, "这周") || strings.Contains(msg, "查询"):
+			code = 1
 			//时间
 			now := time.Now()
 			offset := int(time.Monday - now.Weekday())
@@ -68,10 +81,11 @@ func onKeyPrivateMsg(subType, msgID int32, fromQQ int64, msg string, font int32)
 			}
 			weekStart := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local).AddDate(0, 0, offset)
 			weekEnd := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local).AddDate(0, 0, offset+7)
+			fmt.Println(weekStart, weekEnd)
 			//结果
 			var results []Key
 			//查询
-			db.Find(&results, "created_at BETWEEN ? AND ?", weekStart, weekEnd)
+			db.Order("created_at").Find(&results, "created_at BETWEEN ? AND ?", weekStart, weekEnd)
 			//生成结果
 			if len(results) > 0 {
 				var keys []string
@@ -81,15 +95,53 @@ func onKeyPrivateMsg(subType, msgID int32, fromQQ int64, msg string, font int32)
 				}
 				cqp.SendPrivateMsg(fromQQ, strings.Join(keys, "\n"))
 			} else {
-				cqp.SendPrivateMsg(fromQQ, "本周兑换码:\n暂无兑换码,使用 <兑换码 帮助> 可获取")
+				cqp.SendPrivateMsg(fromQQ, "本周兑换码:\n暂无兑换码,你可以私聊我<兑换码 帮助>来获取操作指令哦")
 			}
-
+		//新增
+		case strings.HasPrefix(msg, "兑换码新增 "):
+			code = 1
+			var key string
+			n, err := fmt.Sscanf(msg, "兑换码新增 %s", &key)
+			if n != 1 || err != nil {
+				cqp.AddLog(cqp.Debug, "兑换码", "兑换码指令不正确: "+msg)
+				return 0
+			}
+			if len(key) > 0 {
+				var old Key
+				db.Find(&old, "key = ?", key)
+				if old.Key != key {
+					db.Create(&Key{Key: key})
+					cqp.SendPrivateMsg(fromQQ, "新增兑换码: "+key)
+				} else {
+					cqp.SendPrivateMsg(fromQQ, "该兑换码已存在!")
+				}
+			}
+		//删除
+		case strings.HasPrefix(msg, "兑换码删除 "):
+			code = 1
+			var key string
+			n, err := fmt.Sscanf(msg, "兑换码删除 %s", &key)
+			if n != 1 || err != nil {
+				cqp.AddLog(cqp.Debug, "兑换码", "兑换码指令不正确: "+msg)
+				return 0
+			}
+			if len(key) > 0 {
+				var old Key
+				db.Find(&old, "key = ?", key)
+				if old.Key == key {
+					db.Delete(&Key{Key: key}, "key = ?", key)
+					cqp.SendPrivateMsg(fromQQ, "删除兑换码: "+key)
+				} else {
+					cqp.SendPrivateMsg(fromQQ, "该兑换码不存在!")
+				}
+			}
 		}
+
 		//帮助
 		if strings.Contains(msg, "帮助") {
+			code = 1
 			sendHelp(true, fromQQ)
 		}
-
 	}
 	return code
 }
@@ -103,6 +155,7 @@ func onKeyGroupMsg(subType, msgID int32, fromGroup, fromQQ int64, fromAnonymous,
 		switch {
 		//查询
 		case strings.Contains(msg, "本周") || strings.Contains(msg, "这周") || strings.Contains(msg, "查询"):
+			code = 1
 			//时间
 			now := time.Now()
 			offset := int(time.Monday - now.Weekday())
@@ -129,6 +182,7 @@ func onKeyGroupMsg(subType, msgID int32, fromGroup, fromQQ int64, fromAnonymous,
 			}
 		//新增
 		case strings.HasPrefix(msg, "兑换码新增 "):
+			code = 1
 			var key string
 			n, err := fmt.Sscanf(msg, "兑换码新增 %s", &key)
 			if n != 1 || err != nil {
@@ -147,6 +201,7 @@ func onKeyGroupMsg(subType, msgID int32, fromGroup, fromQQ int64, fromAnonymous,
 			}
 		//删除
 		case strings.HasPrefix(msg, "兑换码删除 "):
+			code = 1
 			var key string
 			n, err := fmt.Sscanf(msg, "兑换码删除 %s", &key)
 			if n != 1 || err != nil {
@@ -167,6 +222,7 @@ func onKeyGroupMsg(subType, msgID int32, fromGroup, fromQQ int64, fromAnonymous,
 
 		//帮助
 		if strings.Contains(msg, "帮助") {
+			code = 1
 			sendHelp(false, fromGroup)
 		}
 	}
@@ -180,12 +236,16 @@ func sendHelp(single bool, from int64) {
 	help := "兑换码功能:\n" +
 		"1.查询指令☞<兑换码本周/本周兑换码/兑换码查询>\n" +
 		"2.新增指令☞<兑换码新增 [key]>\n" +
-		"兑换码新增 川川这周女装(示例指令)\n" +
-		"3.删除指令☞<兑换码删除 [key]>\n" +
-		"兑换码删除 川川这周女装(示例指令)\n"
+		"3.删除指令☞<兑换码删除 [key]>\n"
 	if single {
 		cqp.SendPrivateMsg(from, help)
 	} else {
 		cqp.SendGroupMsg(from, help)
 	}
+}
+
+/**定时任务*/
+func testJob() {
+	msg := "【滚来滚去】"
+	cqp.SendGroupMsg(816440954, msg)
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/mtdhllf/NinjiaRobot
 go 1.12
 
 require (
-	github.com/Tnze/CoolQ-Golang-SDK v0.0.0-20190904035601-dae5871b9137
-	github.com/jinzhu/gorm v1.9.10
+	github.com/Tnze/CoolQ-Golang-SDK v0.0.0-20191020063555-dd0493defda8
+	github.com/jinzhu/gorm v1.9.11
+	github.com/lib/pq v1.2.0 // indirect
+	github.com/robfig/cron v1.2.0
 	golang.org/x/crypto v0.0.0-20190829043050-9756ffdc2472 // indirect
 )
